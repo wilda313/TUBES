5662bea680f5a13d1cf6bca823227945693c96ef
Wilda Mardhiani <ogida.san@gmail.com>
2017-05-04 16:30:25 +0700

    Add files via upload

---
+++ buatakun.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Buatakun extends CI_Controller {
+
+	function __construct(){
+		parent::__construct();
+		$this->load->model('login_user');
+	}
+	public function index()
+	{
+		$this->template->load('static_login','buatakun');
+	}
+	public function insert(){
+		$data = array(	
+						'username'=> $this->input->post('username'),
+						'Password'=> md5($this->input->post('password')));
+				$insert = $this->login_user->insertAkun('user',$data);
+				if ($insert > 0){
+				redirect(base_url('index.php/login'));
+				}else{
+					echo '</script> gagal di input </script>';
+				}	
+	}
+}
